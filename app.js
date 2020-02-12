aae79f885fb2efb8af7679b937be1421d9f862bf
Jan <jan.ed.pachla@gmail.com>
2020-02-12 14:25:10 -0500

    Fully added user login and user database
    Major update:
    
    Added register and login page
    Added login authentication
    Added individual database for each new user

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
+//libraries for user authentication
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
 //initializing app and using public directory for css
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -17,11 +23,25 @@ app.use(express.static("public"));
 //using ejs for public and views directories containing ejs files
 app.set('view engine', 'ejs');
 
+//initialize sessions for authentication
+app.use(session({
+  secret: "Password required for authenticating receipt tracker app",
+  resave: false,
+  saveUninitialized: false
+}));
+
+//initialize passport and use it with session
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connect to MongoDB and create database
 mongoose.connect("mongodb://localhost:27017/receiptDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+//remove a warning due to passportLocalMongoose
+mongoose.set("useCreateIndex", true);
 
 //structure of an object in the database receiptDB
 const receiptsSchema = {
@@ -33,158 +53,338 @@ const receiptsSchema = {
   price: Number
 };
 
+//structure for new user
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  itemList: [receiptsSchema]
+});
+
+//adding passportLocalMongoose as a plugin
+userSchema.plugin(passportLocalMongoose);
+
+
+//create user model
+const User = mongoose.model("User", userSchema);
+
+//use to create cookies
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //create receipt model
 const Receipt = mongoose.model("Receipt", receiptsSchema);
 
-app.get("/", function(req, res) {
-  Receipt.find({}, function(err, foundReceipt) {
-    res.render("home", {
-      receiptList: foundReceipt
-    });
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/");
+      });
+    }
   });
 });
+
+app.get("/", function(req, res) {
+  if(req.isAuthenticated()){
+    res.render("home", {
+      receiptList: req.user.itemList
+    });
+    //the next few lines of code are used for global DB for testing
+    // Receipt.find({}, function(err, foundReceipt) {
+    //   res.render("home", {
+    //     receiptList: foundReceipt
+    //   });
+    // });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", function(req, res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+      res.redirect("/login");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+
+
+
+
+//////////////////// FIX FRONT END FIRST, THEN TIE DATABASE TO USER, use mongoose subdocuments
 
 
 //display add page
 app.get("/add", function(req, res) {
-  res.render("add");
+  if(req.isAuthenticated()){
+    console.log(req.user.username);
+    res.render("add");
+  } else {
+    res.redirect("/login");
+  }
+
 });
 
 //adds a new item to the receipt list
 app.post("/add", function(req, res) {
-  const newItem = new Receipt({
-    date: req.body.newDate,
-    storeName: req.body.newStore,
-    storeAddress: req.body.newAddress,
-    itemName: req.body.newItem,
-    itemQuantity: req.body.newQuantity,
-    price: req.body.newPrice
-  });
-  if (newItem.date === "" && newItem.storeName === "" && newItem.storeAddress === "" && newItem.itemName === "" && newItem.itemQuantity === null && newItem.price === null) {
-    res.redirect("/add");
-    console.log("nothing has been inputted");
+  if(req.isAuthenticated()){
+    const newItem = new Receipt({
+      date: req.body.newDate,
+      storeName: req.body.newStore,
+      storeAddress: req.body.newAddress,
+      itemName: req.body.newItem,
+      itemQuantity: req.body.newQuantity,
+      price: req.body.newPrice
+    });
+    if (newItem.date === "" && newItem.storeName === "" && newItem.storeAddress === "" && newItem.itemName === "" && newItem.itemQuantity === null && newItem.price === null) {
+      res.redirect("/add");
+      console.log("nothing has been inputted");
+    } else {
+      req.user.itemList.push(newItem);
+      req.user.save(function(err){
+        if(err){
+          console.log(err);
+        }
+      });
+      //next line of code saves in global DB
+      // newItem.save();
+      res.redirect("/");
+      console.log("saved in db");
+    }
   } else {
-    newItem.save();
-    res.redirect("/");
-    console.log("saved in db");
+    res.redirect("/login");
   }
+
 
 });
 
 // display remove page
 app.get("/remove", function(req, res) {
-  res.render("remove");
+  if(req.isAuthenticated()){
+    res.render("remove");
+  } else {
+    res.redirect("/login");
+  }
+
 });
 
 //deletes selected item(s)
 app.post("/remove", function(req, res) {
-  if (typeof(req.body.check) === "string") {
-    const oneItemId = req.body.check;
-    Receipt.findByIdAndRemove(oneItemId, function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Item deleted");
-      }
-    });
-  } else if (typeof(req.body.check) === "object") {
-    const arrayItemId = req.body.check;
-    arrayItemId.forEach(itemId => {
-      Receipt.findByIdAndRemove(itemId, function(err) {
-        if (err) {
+  if(req.isAuthenticated()){
+    if (typeof(req.body.check) === "string") {
+      const oneItemId = req.body.check;
+      req.user.itemList.id(oneItemId).remove();
+      req.user.save(function(err){
+        if(err){
           console.log(err);
         } else {
-          console.log("Item deleted");
+          console.log("Item Deleted");
         }
       });
-    });
+      // Receipt.findByIdAndRemove(oneItemId, function(err) {
+      //   if (err) {
+      //     console.log(err);
+      //   } else {
+      //     console.log("Item deleted");
+      //   }
+      // });
+    } else if (typeof(req.body.check) === "object") {
+      const arrayItemId = req.body.check;
+      arrayItemId.forEach(itemId => {
+        req.user.itemList.id(itemId).remove();
+      });
+        req.user.save(function(err){
+          if(err){
+            console.log(err);
+          } else {
+            console.log("Item Deleted");
+          }
+        });
+        // Receipt.findByIdAndRemove(itemId, function(err) {
+        //   if (err) {
+        //     console.log(err);
+        //   } else {
+        //     console.log("Item deleted");
+        //   }
+        // });
+
+
+      // });
+    }
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
   }
-  res.redirect("/");
+
 });
 
 //displays the modify page
 app.get("/modify", function(req, res) {
-  res.render("modify");
+  if(req.isAuthenticated()){
+    res.render("modify");
+  } else {
+    res.redirect("/login");
+  }
+
 });
 
 //modify selected item(s)
 app.post("/modify", function(req, res) {
-  if (typeof(req.body.check) === "string") {
-    const oneItemId = req.body.check;
-    Receipt.findById(oneItemId, function(err, item) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("The item by id is : " + item);
-        res.render("modify", {
-          receiptList: [item]
-        });
-      }
-    });
-  } else if (typeof(req.body.check) === "object") {
-    const arrayItemId = req.body.check;
+  if(req.isAuthenticated()){
+    if (typeof(req.body.check) === "string") {
+      const oneItemId = req.body.check;
+      // Receipt.findById(oneItemId, function(err, item) {
+      //   if (err) {
+      //     console.log(err);
+      //   } else {
+      //     console.log("The item by id is : " + item);
+      //     res.render("modify", {
+      //       receiptList: [item]
+      //     });
+      //   }
+      // });
+      res.render("modify", {
+        receiptList: [req.user.itemList.id(oneItemId)]
+      });
+    } else if (typeof(req.body.check) === "object") {
+      const arrayItemId = req.body.check;
+      let arrayItem = [];
 
-    console.log(arrayItemId);
+      arrayItemId.forEach(itemId => {
+        arrayItem.push(req.user.itemList.id(itemId));
+      });
+      res.render("modify", {
+        receiptList: arrayItem
+      });
 
-    Receipt.find().where('_id').in(arrayItemId).exec(function(err, fullList) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("modify", {
-          receiptList: fullList
-        });
-      }
-    });
+      // console.log(arrayItemId);
+      //
+      // Receipt.find().where('_id').in(arrayItemId).exec(function(err, fullList) {
+      //   if (err) {
+      //     console.log(err);
+      //   } else {
+      //     res.render("modify", {
+      //       receiptList: fullList
+      //     });
+      //   }
+      // });
+    } else {
+      res.redirect("/");
+    }
   } else {
-    res.redirect("/");
+    res.redirect("/login");
   }
+
 });
 
 //updates in the database the changed values
 app.post("/submitModified", function(req, res) {
-  console.log(req.body);
-  let counter = 0;
-  console.log(typeof(req.body.idItem));
-  if (typeof(req.body.idItem) === "string") {
-    Receipt.findByIdAndUpdate({
-      _id: req.body.idItem
-    }, {
-      date: req.body.updatedDate,
-      storeName: req.body.updatedStoreName,
-      storeAddress: req.body.updatedStoreAddress,
-      itemName: req.body.updatedItem,
-      itemQuantity: req.body.updatedQuantity,
-      price: req.body.updatedPrice
-    }, function(err, result) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(result);
-      }
-    });
-  } else {
-    req.body.idItem.forEach(id => {
-      Receipt.findByIdAndUpdate({
-        _id: id
-      }, {
-        date: req.body.updatedDate[counter],
-        storeName: req.body.updatedStoreName[counter],
-        storeAddress: req.body.updatedStoreAddress[counter],
-        itemName: req.body.updatedItem[counter],
-        itemQuantity: req.body.updatedQuantity[counter],
-        price: req.body.updatedPrice[counter]
-      }, function(err, result) {
-        if (err) {
+  if(req.isAuthenticated()){
+    console.log(req.body);
+    let counter = 0;
+    console.log(typeof(req.body.idItem));
+    if (typeof(req.body.idItem) === "string") {
+      req.user.itemList.id(req.body.idItem).date = req.body.updatedDate;
+      req.user.itemList.id(req.body.idItem).storeName = req.body.updatedStoreName;
+      req.user.itemList.id(req.body.idItem).storeAddress = req.body.updatedStoreAddress;
+      req.user.itemList.id(req.body.idItem).itemNamename = req.body.updatedItem;
+      req.user.itemList.id(req.body.idItem).itemQuantity = req.body.updatedQuantity;
+      req.user.itemList.id(req.body.idItem).price = req.body.updatedPrice;
+
+      req.user.save(function(err){
+        if(err){
           console.log(err);
         } else {
-          console.log(result);
+          console.log("Item Deleted");
         }
       });
-      counter++;
-    });
+      // Receipt.findByIdAndUpdate({
+      //   _id: req.body.idItem
+      // }, {
+      //   date: req.body.updatedDate,
+      //   storeName: req.body.updatedStoreName,
+      //   storeAddress: req.body.updatedStoreAddress,
+      //   itemName: req.body.updatedItem,
+      //   itemQuantity: req.body.updatedQuantity,
+      //   price: req.body.updatedPrice
+      // }, function(err, result) {
+      //   if (err) {
+      //     console.log(err);
+      //   } else {
+      //     console.log(result);
+      //   }
+      // });
+
+
+
+
+      } else {
+        req.body.idItem.forEach(id => {
+        req.user.itemList.id(id).date = req.body.updatedDate[counter];
+        req.user.itemList.id(id).storeName = req.body.updatedStoreName[counter];
+        req.user.itemList.id(id).storeAddress = req.body.updatedStoreAddress[counter];
+        req.user.itemList.id(id).itemName = req.body.updatedItem[counter];
+        req.user.itemList.id(id).itemQuantity = req.body.updatedQuantity[counter];
+        req.user.itemList.id(id).price = req.body.updatedPrice[counter];
+        // Receipt.findByIdAndUpdate({
+        //   _id: id
+        // }, {
+        //   date: req.body.updatedDate[counter],
+        //   storeName: req.body.updatedStoreName[counter],
+        //   storeAddress: req.body.updatedStoreAddress[counter],
+        //   itemName: req.body.updatedItem[counter],
+        //   itemQuantity: req.body.updatedQuantity[counter],
+        //   price: req.body.updatedPrice[counter]
+        // }, function(err, result) {
+        //   if (err) {
+        //     console.log(err);
+        //   } else {
+        //     console.log(result);
+        //   }
+        // });
+        counter++;
+      });
+      req.user.save(function(err){
+        if(err){
+          console.log(err);
+        } else {
+          console.log("Item Deleted");
+        }
+      });
+    }
+    counter = 0;
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
   }
-  counter = 0;
-  res.redirect("/");
+
 });
 
 
